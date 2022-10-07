d72de2eb4c2ee8b38da9e1cb4344d60418ee874c
agil17 <agil17@calstatela.edu>
2022-10-07 15:06:52 -0700

    Add solution to exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    const arr2 = [];
+    for(const x of arr) {
+        if(args.includes(x) === false){
+            arr2.push(x);
+        }
+    }
+    return arr2;
 };
+
+removeFromArray([1,2,3,4],3,'4');
 
 // Do not edit below this line
 module.exports = removeFromArray;
