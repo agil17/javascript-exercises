c5b74a3e6ded96986a17530cd82d984e17da37e7
agil17 <agil17@calstatela.edu>
2022-10-07 15:32:32 -0700

    Add solution to exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if(typeof start === 'number' && 
+       typeof end === 'number' &&
+       start > -1 &&
+       end > -1
+    ) {
+        if(start < end) {
+            for(let i = start; i <= end; i++) {
+                sum = sum + i;
+            }
+        }
+        else {
+            for(let i = end; i <= start; i++) {
+                sum = sum + i;
+            }
+        }
+        return sum;
+    }
+    else {
+        return 'ERROR';
+    }
 };
+
+sumAll(-1,2);
 
 // Do not edit below this line
 module.exports = sumAll;
