a19ce7f712c9611dcf70351bb7c54a960ebc8ed8
agil17 <agil17@calstatela.edu>
2022-10-07 13:54:23 -0700

    Add solution to exercise 03
    All tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let str2 = "";
+    for(let i = str.length - 1; i >= 0; i--) {
+        str2 = str2.concat(str[i]);
+    }
+    return str2;
+    
 };
+
+reverseString('hello world');
 
 // Do not edit below this line
 module.exports = reverseString;
