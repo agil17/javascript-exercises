5641afd7b4e7e7c3726ac4739613faee6ab7509f
agil17 <agil17@calstatela.edu>
2022-10-07 13:33:01 -0700

    Add Solution to exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let str2 = "";
+    if(num < 0){
+        return "ERROR";
+    }
+    else {
+        for(let i = 0; i < num; i++) {
+            str2 = str + str2
+        }
+    }
+    return str2;
 };
 
 // Do not edit below this line
